18ad8dfed7bf4113077b8616b66bedfc0f82a731
Mezzadra, Ignacio (Nacho) <imezzadra@avaya.com>
2015-09-27 11:58:43 -0300

    Expose 'webapps' and 'logs' directories

--- docker-images/oracle-jdk7-tomcat-7/run.sh
+++ docker-images/oracle-jdk7-tomcat-7/run.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-sudo docker run --rm=true -p 8080 -t -i --name tomcat7 jdk7/tomcat7
+sudo docker run -d -p 8080:8080 -t -i --name tomcat7 -v /home/imezzadra/workspace/docker/docker-images/webapps/:/opt/tomcat/webapps jdk7/tomcat7
