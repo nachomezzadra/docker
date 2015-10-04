67255740e3c20e21eb0c60235410fe3c318dbcbd
Nacho Mezzadra <nachomezzadra@gmail.com>
2015-10-03 22:10:48 -0300

    Enhance script to determine IP address container and boot2docker vm

--- util-scripts/determine-ip-address.sh
+++ util-scripts/determine-ip-address.sh
@@ -1,3 +1,14 @@
 #/bin/bash
 
-docker inspect --format '{{ .NetworkSettings.IPAddress }}' tomcat7
+if [[ -z $1 ]]; then
+	echo -e "\n*** Image Name missing.\nUsage: $0 <image-name>\n"
+	exit 1
+fi
+
+echo "-- Container Ip address --"
+docker inspect --format '{{ .NetworkSettings.IPAddress }}' $1
+
+echo -e "\n-- Kitematic ip address --"
+docker-machine ip default
+
+exit 0
